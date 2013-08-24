06bf914167e2411e620f17658d10502f7ef23546
Christopher Wade <cjacobwade@gmail.com>
2013-08-24 17:25:39 -0500

    Base model, shader stuff, added buff guy picture, player script

--- LD27/Assets/Scripts/Player.cs
+++ LD27/Assets/Scripts/Player.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-
+	
+	CharacterController cc;
+	public int moveSpeed;
+	
 	// Use this for initialization
 	void Start () {
-	
+		cc = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		cc.Move(new Vector3(0,0,moveSpeed*Time.deltaTime));
 	}
 }
---
+++ LD27/Assets/Shaders/Cape.shader
@@ -0,0 +1,21 @@
+﻿// Unlit shader. Simplest possible textured shader.
+// - no lighting
+// - no lightmap support
+// - no per-material color
+
+Shader "Unlit/Texture" {
+Properties {
+	_MainTex ("Base (RGB)", 2D) = "white" {}
+}
+
+SubShader {
+	Tags { "RenderType"="Opaque" }
+	LOD 100
+	Cull Off
+	Pass {
+		Lighting Off
+		SetTexture [_MainTex] { combine texture } 
+	}
+	
+}
+}
